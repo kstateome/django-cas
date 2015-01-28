443ea5ccc960b8ddf75c65be410daffa85e9db88
Derek Stegelman <dstegelman@gmail.com>
2015-01-28 12:20:28 -0700

    Test update.

--- cas/tests/test_backend.py
+++ cas/tests/test_backend.py
@@ -1,7 +1,16 @@
+import mock
 from django.test import TestCase
+
+from cas.backends import CASBackend
+from cas.tests import factories
 
 
 class CASBackendTest(TestCase):
 
-    def test_this(self):
-        self.fail()
+    def setUp(self):
+        self.user = factories.UserFactory.create()
+
+    def test_get_user(self):
+        backend = CASBackend()
+
+        self.assertEqual(backend.get_user(self.user.pk), self.user)
