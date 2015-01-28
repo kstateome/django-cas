b6ac45aa5099f9ed0120f50817745a5d3be3fc12
Derek Stegelman <dstegelman@gmail.com>
2015-01-28 07:24:52 -0700

    Initial travis.

---
+++ run_tests.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os, sys
+from django.conf import settings
+import django
+
+DIRNAME = os.path.dirname(__file__)
+
+if django.VERSION[1] < 4:
+    # If the version is NOT django 4 or greater
+    # then remove the TZ setting.
+
+    settings.configure(DEBUG=True,
+                       DATABASES={
+                           'default': {
+                               'ENGINE': 'django.db.backends.sqlite3',
+                               }
+                       },
+                       ROOT_URLCONF='mailqueue.urls',
+                       INSTALLED_APPS=('django.contrib.auth',
+                                       'django.contrib.contenttypes',
+                                       'django.contrib.sessions',
+                                       'django.contrib.admin',
+                                       'cas',))
+else:
+    settings.configure(DEBUG=True,
+                       DATABASES={
+                           'default': {
+                               'ENGINE': 'django.db.backends.sqlite3',
+                               }
+                       },
+                       ROOT_URLCONF='mailqueue.urls',
+                       INSTALLED_APPS=('django.contrib.auth',
+                                       'django.contrib.contenttypes',
+                                       'django.contrib.sessions',
+                                       'django.contrib.admin',
+                                       'cas',),
+                       USE_TZ=True)
+
+
+try:
+    # Django 1.7 needs this, but other versions dont.
+    django.setup()
+except AttributeError:
+    pass
