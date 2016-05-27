259273fe44e7498223ea2b241c5f9f8bbea6fa04
Derek Stegelman <dstegelman@gmail.com>
2016-05-27 13:39:13 -0600

    Test for referer.

--- cas/tests/test_views.py
+++ cas/tests/test_views.py
@@ -15,6 +15,17 @@ class RequestFactoryRemix(RequestFactory):
         return False
 
 
+class SecureRequestFactory(RequestFactory):
+
+    path = '/'
+
+    def get_host(self):
+        return 'signin.k-state.edu'
+
+    def is_secure(self):
+        return True
+
+
 class CASViewsTestCase(TestCase):
 
     def setUp(self):
@@ -37,6 +48,14 @@ class CASViewsTestCase(TestCase):
 
         self.request.GET = {'next': 'foo'}
         self.assertEqual(_redirect_url(self.request), 'foo')
+
+    @override_settings(CAS_IGNORE_REFERER=False)
+    @override_settings(CAS_REDIRECT_URL=None)
+    def test_https_redirect_url(self):
+        self.request = SecureRequestFactory()
+        self.request.GET = {}
+        self.request.META = {'HTTP_REFERER': 'https://example.com/'}
+        self.assertEqual(_redirect_url(self.request), 'https://example.com/')
 
     def test_login_url(self):
         self.assertEqual(_login_url('http://localhost:8000/accounts/login/'),
