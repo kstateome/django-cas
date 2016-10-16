fc82f0b41c43c7842e3dc1df2c9e7fbc3317b2c0
Derek Stegelman <dstegelman@gmail.com>
2016-10-16 08:18:56 -0600

    Use new middleware where available, otherwise continue to use object as a base.

--- cas/middleware.py
+++ cas/middleware.py
@@ -5,6 +5,15 @@ try:
 except ImportError:
     from urllib.parse import urlencode
 
+
+MIDDLEWARE_BASE = None
+
+try:
+    from django.utils.deprecation import MiddlewareMixin
+    MIDDLEWARE_BASE = MiddlewareMixin
+except ImportError:
+    MIDDLEWARE_BASE = object
+
 from django.conf import settings
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.auth import logout as do_logout
@@ -19,7 +28,7 @@ from cas.views import login as cas_login, logout as cas_logout
 __all__ = ['CASMiddleware']
 
 
-class CASMiddleware(object):
+class CASMiddleware(MIDDLEWARE_BASE):
     """
     Middleware that allows CAS authentication on admin pages
     """
