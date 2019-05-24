4bce9023d503b245e27deaff0ea257c81d4a7104
Jordan Reiter (dev.editlib.org) <jordan@aace.org>
2019-05-24 14:27:23 -0400

    Add callback function for server url, route to different login servers based on service value

--- cas/utils.py
+++ cas/utils.py
@@ -1,7 +1,7 @@
 import logging
 
 from django.conf import settings
-
+from django.utils.module_loading import import_string
 
 logger = logging.getLogger(__name__)
 
@@ -24,3 +24,20 @@ def cas_response_callbacks(tree):
             logger.error("Attribute Error: %s" % e)
             raise e
         func(tree)
+
+def get_cas_server_url(service):
+    try:
+        cas_server_url_callback = settings.CAS_SERVER_URL_CALLBACK
+    except AttributeError:
+        pass
+    else:
+        try:
+            callback = import_string(cas_server_url_callback)
+        except ImportError:
+            raise RuntimeError(
+                "Invalid callback for CAS_SERVER_URL_CALLBACK: {}".format(
+                    cas_server_url_callback
+                )
+            )
+        return callback(service)
+    return settings.CAS_SERVER_URL
